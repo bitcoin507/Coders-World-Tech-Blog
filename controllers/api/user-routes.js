abe590c770e069a8aba8779fd67def9f4acebcfd
Craig <panamaworldwideinvestments@gmail.com>
2022-06-25 10:20:12 +0100

    added routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,108 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../../models/User');
+
+// CREATE a new user
+router.post('/signUp', async (req, res) => {
+  const confirm = await User.find({Username : req.body.username ,email : req.body.email})
+  confirm && res.status(400).json('this user or email already exist');
+  try {
+    const newUser = req.body;
+    const salt = await bcrypt.genSalt(10);
+    // hash the password from 'req.body' and save to newUser
+    newUser.password = await bcrypt.hash(req.body.password, 10);
+    // create the newUser with the hashed password and save to DB
+    const userData = await User.create(newUser);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//Login
+router.post('/login', async (req, res) => {          
+    try {  
+      const { email, password } = req.body;
+      const userData = await User.findOne({
+        where: { email },
+      });
+      if (!userData) {
+        res.status(400).json({ message: 'Invalid Credentials' });
+        return;
+      }
+      const isPasswordValid = await bcrypt.compare(password, userData.password);
+      if (!isPasswordValid) {
+        res.status(400).json({ message: 'Invalid Credentials' });
+        return;
+      }
+      req.session.logged_in = true;
+      req.session.user_id = userData.id;
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    } 
+  })
+
+    //Logout
+    router.get('/logout', (req, res) => {
+        req.session.destroy();  // destroy the session
+        res.status(200).json({ message: 'Logged out successfully' });
+        } )   
+        
+      
+            
+
+
+//UPDATE a user
+router.put('/:updateUser', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (user.password === req.body.password) {
+            await User.updateOne({ $set: req.body });
+            res.status(200).json('user updated');
+        } else {
+            res.status(403).json('User Info does not match');
+        }
+    } catch (error) { res.status(500).json(error)
+     }
+})  
+
+//Delete a user
+router.delete('/:deleteUser', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (user.password === req.body.password) {
+            await User.deleteOne({ _id: req.params.id });
+            res.status(200).json('user deleted');
+        } else {
+            res.status(403).json('User Info does not match');
+        }
+    } catch (error) { res.status(500).json(error)
+        }
+})
+
+
+
+//get All users
+router.get('/searchUsers', async (req,res) => {
+    try {
+     const users = await Post.find();
+    res.status(200).json(users);
+  
+    } catch (error) {
+     res.status(500).json(error); 
+    }
+  })
+//Get user by id
+  router.get('/:userName', async (req,res) => {
+    try {
+     const user = await Post.findById(req.params.id);
+    res.status(200).json(user);
+    
+} catch (error) {
+     res.status(500).json(error); 
+    }
+  })
+
+
+module.exports = router;
