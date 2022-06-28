07c69fdcb1d99138617102eab7d7d45f1bdd26e6
Craig <panamaworldwideinvestments@gmail.com>
2022-06-28 13:10:12 +0100

    updated

--- models/index.js
+++ models/index.js
@@ -3,6 +3,7 @@ const Category = require('./Category');
 const Post = require('./Post');
 const Popular = require('./Popular');
 
+
 Popular.belongsTo(User, { 
   foreignKey: 'user_id',
    
@@ -19,12 +20,7 @@ Popular.belongsTo(Post, {
   targetKey: 'id'
   } );
 
-Popular.hasMany(Comment, {
-  foreignKey: 'popular_id',
-  onDelete: 'CASCADE',
-  targetKey: 'id'
-  
-} );
+
 
 Category.hasMany(Popular, {
   foreignKey: 'category_id',
@@ -56,22 +52,12 @@ Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: 'CASCADE',
-});
 
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-});
 
-Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-});
 
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
 
-module.exports = { User, Post, Category, Comment,Popular };
+
+
+
+
+module.exports = { User, Post, Category, Popular };
