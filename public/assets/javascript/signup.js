a3582052eda2282ecae005bba5e5588e1643e5ef
Craig <panamaworldwideinvestments@gmail.com>
2022-07-01 23:12:50 +0100

    created sign in modal as a partial

--- public/assets/javascript/signup.js
+++ public/assets/javascript/signup.js
@@ -1,5 +1,7 @@
-// Get the modal
-var modal = document.getElementById('id01');
+// Get the signup modal
+var modal = document.getElementById('id01');  
+var logInModal = document.getElementById('id02');
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
